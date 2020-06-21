3696137f657b34158e8917b34f4808ef5964d7f1
CrisTo <toaquizacris@hotmail.com>
2020-06-20 21:40:44 -0500

    ui: Add css loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
       }
     ]
   }
